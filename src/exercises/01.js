fbd3aba8d4303303c7846bd6c9389514e9ab7ab0
kanastasoff <kireprogramista@gmail.com>
2021-02-08 09:39:42 +0200

    advanced react hooks useReducer

---
+++ src/exercises/01.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+function countReducer(state,newState) {
+  return newState
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [count, setCount] = React.useReducer(countReducer,initialCount)
+
+  const increment = () => setCount(count + step)
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App 
