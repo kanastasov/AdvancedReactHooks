3f0245e7d5eabbd950838c1b514c0b0f7cd133a0
kanastasoff <kireprogramista@gmail.com>
2021-02-10 13:36:08 +0200

    extra 3

---
+++ src/exercises/01.extra-3.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+const countReducer = (state, action) => ({
+  ...state,
+  ...(typeof action === 'function' ? action(state) : action),
+})
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
